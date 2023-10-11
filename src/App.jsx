e3d1ad851be6e3274f919ea9b6330cf7be16c9df
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2023-10-11 15:29:51 -0700

    css

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="bg-site bg-no-repeat bg-cover overflow-hidden mx-auto container">
       <Menu />
-      <div className='h-[4000px]'></div>
+      
     </div>
       
     
