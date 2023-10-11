89d8f769b75311f4528771d02cc0e93fcba6046e
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2023-10-11 13:35:12 -0700

    title fix

--- src/components/Eyelashes.jsx
+++ src/components/Eyelashes.jsx
@@ -3,7 +3,7 @@ import { eyelashServices } from '../ServicesData';
 const Eyelashes = () => {
   return (
     <div className="container mx-auto flex flex-col items-center">
-      <h2 className="font-bold text-lg">Facial Services</h2>
+      <h2 className="font-bold text-lg">Eyelash Services</h2>
       <ul className="flex flex-col items-center max-w-md">
         {eyelashServices.map((service, index) => (
           <li key={index} className=" flex justify-between w-full p-2 text-center">
--- src/components/Wax.jsx
+++ src/components/Wax.jsx
@@ -3,7 +3,7 @@ import { waxingServices } from '../ServicesData';
 const Wax = () => {
   return (
     <div className="container mx-auto flex flex-col items-center">
-      <h2 className="font-bold text-lg">Facial Services</h2>
+      <h2 className="font-bold text-lg">Wax Services</h2>
       <ul className="flex flex-col items-center max-w-md">
         {waxingServices.map((service, index) => (
           <li key={index} className=" flex justify-between w-full p-2 text-center">
