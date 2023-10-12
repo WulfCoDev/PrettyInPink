cf8e24342604ff2676dbe86848361918c4cbf9d4
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2023-10-12 12:27:47 -0700

    css

--- src/components/Wax.jsx
+++ src/components/Wax.jsx
@@ -6,9 +6,9 @@ const Wax = () => {
       <h2 className="font-bold text-2xl">Waxing Services</h2>
       <ul className="flex flex-col items-center max-w-md">
         {waxingServices.map((service, index) => (
-          <li key={index} className=" flex justify-between w-full text-center flex-col">
+          <li key={index} className=" flex justify-between w-full text-center flex-col items-center mb-3">
             <div className='flex'>
-              <p className="text-lg flex justify-start">{service.serviceName} </p>
+              <p className="text-lg flex">{service.serviceName} </p>
             <p className='font-bold text-xs flex items-center'> {service.price}</p>
             </div>
             
