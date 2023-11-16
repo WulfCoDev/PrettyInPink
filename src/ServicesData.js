eeea5558d5187b254fcbb6a82920d86cb959476f
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2023-11-16 13:51:13 -0800

    expanding card test

--- src/ServicesData.js
+++ src/ServicesData.js
@@ -6,30 +6,35 @@ export const facialServices = [
       serviceLength: "60 min",
       serviceType: "Facials",
       price: 60,
+      description: "This service includes Pre Cleanse, Cleanser, Exfoliant, Dermaplane, Hydrating Mask, Hydrating Moisturizer, Mineral Sunscreen.",
     },
     {
       serviceName: "Microdermabrasion",
       serviceLength: "30 or 60 min",
       serviceType: "Facials",
       price: 75,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
     },
     {
         serviceName: "Chemical Peel",
         serviceLength: "30 or 60 min",
         serviceType: "Facials",
         price: 75,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
       },
       {
         serviceName: "Customizable",
         serviceLength: "30 or 60 min",
         serviceType: "Facials",
         price: 75,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
       },
       {
         serviceName: "High-Frequency",
         serviceLength: "30 or 60 min",
         serviceType: "Facials",
         price: 75,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
       },
     // Add more facial services
   ];
@@ -40,42 +45,49 @@ export const facialServices = [
       serviceType: "Waxing",
       serviceLength: "10 - 15 min",
       price: 20,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
     },
     {
       serviceName: "Leg",
       serviceType: "Waxing",
       serviceLength: "30 - 45 min",
       price: 40,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
     },
     {
       serviceName: "Arm",
       serviceType: "Waxing",
       serviceLength: "20 - 30 min",
       price: 40,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
     },
     {
       serviceName: "Lip",
       serviceType: "Waxing",
       serviceLength: "10 - 15 min",
       price: 40,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
     },
     {
       serviceName: "Underarm",
       serviceType: "Waxing",
       serviceLength: "10 - 15 min",
       price: 40,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
     },
     {
       serviceName: "Nose",
       serviceType: "Waxing",
       serviceLength: "10 - 15 min",
       price: 40,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
     },
     {
       serviceName: "Stomach",
       serviceType: "Waxing",
       serviceLength: "10 - 15 min",
       price: 40,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
     },
     // Add more waxing services
   ];
@@ -86,12 +98,14 @@ export const facialServices = [
       serviceType: "Eyelashes",
       serviceLength: "30 min",
       price: 80,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
     },
     {
       serviceName: "Lash Lift & Tint",
       serviceType: "Eyelashes",
       serviceLength: "30 - 60 min",
       price: 100,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id ornare arcu odio ut sem. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
     },
     // Add more eyelash services
   ];  
--- src/components/Facials.jsx
+++ src/components/Facials.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { facialServices } from '../ServicesData';
 
 const Facials = () => {
+  const [expandedItem, setExpandedItem] = useState(null);
+
+  const handleItemClick = (index) => {
+    setExpandedItem((prev) => (prev === index ? null : index));
+  };
+
   return (
     <div className="container mx-auto flex flex-col items-center mb-6 text-white">
       <h2 className="font-bold text-3xl font-primary">Facial Services</h2>
       <ul className="flex flex-col items-center max-w-md">
         {facialServices.map((service, index) => (
-          <li key={index} className=" flex justify-between w-full text-center flex-col items-center mb-3">
-            <div className='flex'>
-              <p className="text-xl flex font-tertiary">{service.serviceName} </p>
-            
-            </div>
-            <div className='flex gap-x-1'>
-              <p className='text-xs flex justify-start font-tertiary'>{service.serviceLength} | </p>
-              <p className='font-bold text-xs flex items-center font-tertiary'> ${service.price}</p>
-              
-            </div>
+          <li
+            key={index}
+            className={`w-full text-center flex justify-center flex-col items-center mb-3 h-[${expandedItem === index ? 'auto' : '50'}px] overflow-hidden`}
+          >
+            <button
+              className="flex flex-col justify-center items-center cursor-pointer"
+              onClick={() => handleItemClick(index)}
+            >
+              <div className="flex">
+                <p className="text-xl flex font-tertiary">{service.serviceName} </p>
+              </div>
+              <div className="flex gap-x-1">
+                <p className="text-xs flex justify-start font-tertiary">{service.serviceLength} | </p>
+                <p className="font-bold text-xs flex items-center font-tertiary"> ${service.price}</p>
+              </div>
+              {expandedItem === index && (
+                <div className="text-xs mt-2">
+                  <p className='font-tertiary'>{service.description}</p>
+                </div>
+              )}
+            </button>
           </li>
         ))}
       </ul>
