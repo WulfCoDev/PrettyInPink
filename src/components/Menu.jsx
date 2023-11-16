9c9c37b6bfd5f83fc09974972de870b6ad0a5eb4
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2023-11-16 15:43:23 -0800

    bg test

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,7 +6,7 @@ const Menu = () => {
 
     return (
         <div>
-            <div className="container mx-auto border-solid border-white border-2 m-6 shadow-xl bg-pink-500/30">
+            <div className="container mx-auto border-solid border-white border-2 m-6 shadow-xl bg-pink-500/40">
                 {/* Title */}
                 <div className="flex justify-center items-center m-6">
                     <h1 className="text-white shadow-neonpink font-secondary text-4xl lg:text-6xl uppercase tracking-wider text-center">Prettie in Pink</h1>
