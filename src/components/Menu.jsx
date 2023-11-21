f9923d2770f65f8371b4818244d6cade6f7865d2
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2023-11-21 13:54:12 -0800

    css

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,13 +7,15 @@ const Menu = () => {
 
     return (
         <div>
-            <div className="container mx-auto border-solid border-white border-2 m-6 bg-white/10">
+            <div className="container mx-auto border-solid border-white border-2 m-6 bg-black/10">
                 {/* Title */}
-                <div className="flex justify-center items-center m-2 h-[200px] mb-4">
+                <div className="flex justify-center items-center m-2 h-[200px]">
                     <h1 className="neonText text-white font-fourth text-6xl lg:text-6xl uppercase tracking-wider text-center">Prettie <br/> in <br/> Pink</h1>
                 </div>
                 {/* Image */}
-                
+                <div className="flex justify-center items-center">
+                    <img src="src/assets/header.png" />
+                </div>
 
                 {/* Services */}
                 <div>
