c779d564659261527b116a96fef4827af5181918
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2023-11-21 16:16:07 -0800

    css

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,7 +9,7 @@ const Menu = () => {
         <div>
             <div className="container mx-auto border-solid border-white border-2 m-6 bg-white/10">
                 {/* Title */}
-                <div className="flex font-seventh text-[4.5rem] justify-center items-center h-[400px] flex-col">
+                <div className="flex font-seventh text-[4rem] justify-center items-center h-[400px] flex-col">
                     <h1 className="neonText lg:text-6xl uppercase text-center h-[100px]">Prettie</h1>
                     <h1 className="neonText lg:text-6xl uppercase text-center h-[100px]">in</h1>
                     <h1 className="neonText lg:text-6xl uppercase text-center h-[100px]">Pink</h1>
